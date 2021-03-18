0266f6c6cb18d70412ddc33160aeadfa2eb5f8da
tonezone6 <tonezone6@gmail.com>
2021-03-18 12:55:59 +0100

    Initial Commit

---
+++ JokeFetcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JokeFetcher
+//
+//  Created by Alex Stratu on 18.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JokeFetcher/JokeFetcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFetcherApp.swift
+//  JokeFetcher
+//
+//  Created by Alex Stratu on 18.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct JokeFetcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
